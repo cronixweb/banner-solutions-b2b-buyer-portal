2b9690600f6c03e39b061a7e5e2221104a1d4f77
Harsharaj Shisodiya <harsharaj.shisodiya@cronixweb.com>
2025-03-27 13:13:28 +0530

    fix | CORS issue

--- apps/storefront/vite.config.ts
+++ apps/storefront/vite.config.ts
@@ -29,6 +29,13 @@ export default defineConfig(({ mode }) => {
     },
     server: {
       port: 3001,
+      cors: {
+        origin: 'https://banner-solutions-b2b-site.vercel.app',
+        methods: ['GET', 'POST'],
+      },
+      headers: {
+        'Access-Control-Allow-Origin': 'https://banner-solutions-b2b-site.vercel.app'
+      },
       proxy: {
         '/bigcommerce': {
           target:
